ee4ec52dde64dec8a83f3231b912791f9263a54e
yhappy <yhappy_2008@126.com>
2026-02-05 17:16:37 +0800

    UI

--- pages/map/config/city-config.js
+++ pages/map/config/city-config.js
@@ -437,62 +437,62 @@ const CITY_CONFIG = {
     name: '三明',
     titleImage: 'https://app5.fjsen.com/h5/20260122/images/三明title.png',
     mapImage: 'https://app5.fjsen.com/h5/20260122/images/三明map.png',
-    textColumnsTop: '1150rpx', // 底部文字区域的top值
+    textColumnsTop: '1050rpx', // 底部文字区域的top值
     leftColumnCount: 4, // 左列文字条目数
     items: [{
         id: 1,
         content: '三明泰宁环大金湖观光热气球',
         dot: {
-          top: '480rpx',
-          left: '580rpx'
+          top: '510rpx',
+          left: '440rpx'
         }
       },
       {
         id: 2,
         content: '三元区棠园·大众茶馆',
         dot: {
-          top: '520rpx',
-          left: '450rpx'
+          top: '700rpx',
+          left: '500rpx'
         }
       },
       {
         id: 3,
         content: '三明醉有才大食堂',
         dot: {
-          top: '560rpx',
-          left: '400rpx'
+          top: '750rpx',
+          left: '550rpx'
         }
       },
       {
         id: 4,
         content: '福建（泰宁） 乡村非物质文化遗产博览苑',
         dot: {
-          top: '600rpx',
-          left: '620rpx'
+          top: '710rpx',
+          left: '150rpx'
         }
       },
       {
         id: 5,
         content: '万寿岩遗址博物馆数字化展厅',
         dot: {
-          top: '500rpx',
-          left: '350rpx'
+          top: '670rpx',
+          left: '300rpx'
         }
       },
       {
         id: 6,
         content: '泰宁耕读李家森林风景道',
         dot: {
-          top: '540rpx',
-          left: '550rpx'
+          top: '820rpx',
+          left: '420rpx'
         }
       },
       {
         id: 7,
         content: '中央苏区反" 围剿"纪念馆',
         dot: {
-          top: '580rpx',
-          left: '280rpx'
+          top: '460rpx',
+          left: '100rpx'
         }
       }
     ]
@@ -501,54 +501,54 @@ const CITY_CONFIG = {
     name: '莆田',
     titleImage: 'https://app5.fjsen.com/h5/20260122/images/莆田title.png',
     mapImage: 'https://app5.fjsen.com/h5/20260122/images/莆田map.png',
-    textColumnsTop: '1150rpx', // 底部文字区域的top值
+    textColumnsTop: '1080rpx', // 底部文字区域的top值
     leftColumnCount: 3, // 左列文字条目数
     items: [{
         id: 1,
         content: '《印象·妈祖》文旅演艺',
         dot: {
-          top: '720rpx',
-          left: '480rpx'
+          top: '770rpx',
+          left: '510rpx'
         }
       },
       {
         id: 2,
         content: '莆田兴化府历史文化街区县巷"非遗一条街"',
         dot: {
-          top: '640rpx',
-          left: '420rpx'
+          top: '460rpx',
+          left: '280rpx'
         }
       },
       {
         id: 3,
         content: '湄洲岛泊澜清风民宿',
         dot: {
-          top: '780rpx',
-          left: '520rpx'
+          top: '620rpx',
+          left: '160rpx'
         }
       },
       {
         id: 4,
         content: '木兰溪"水上巴士"',
         dot: {
-          top: '580rpx',
-          left: '380rpx'
+          top: '480rpx',
+          left: '100rpx'
         }
       },
       {
         id: 5,
         content: '莆田盐场',
         dot: {
-          top: '680rpx',
-          left: '580rpx'
+          top: '590rpx',
+          left: '450rpx'
         }
       },
       {
         id: 6,
         content: '湄洲岛妈祖文化旅游区',
         dot: {
-          top: '740rpx',
-          left: '500rpx'
+          top: '790rpx',
+          left: '390rpx'
         }
       }
     ]
@@ -557,22 +557,22 @@ const CITY_CONFIG = {
     name: '南平',
     titleImage: 'https://app5.fjsen.com/h5/20260122/images/南平title.png',
     mapImage: 'https://app5.fjsen.com/h5/20260122/images/南平map.png',
-    textColumnsTop: '1250rpx', // 底部文字区域的top值
+    textColumnsTop: '1100rpx', // 底部文字区域的top值
     leftColumnCount: 6, // 左列文字条目数
     items: [{
         id: 1,
         content: '南平武夷山国家公园六曲揽胜观景平台',
         dot: {
-          top: '560rpx',
-          left: '580rpx'
+          top: '580rpx',
+          left: '540rpx'
         }
       },
       {
         id: 2,
         content: '武夷山市半米茶席',
         dot: {
-          top: '600rpx',
-          left: '520rpx'
+          top: '790rpx',
+          left: '220rpx'
         }
       },
       {
@@ -580,79 +580,79 @@ const CITY_CONFIG = {
         content: '武夷山市世界红茶小镇桐木村',
         dot: {
           top: '640rpx',
-          left: '620rpx'
+          left: '600rpx'
         }
       },
       {
         id: 4,
         content: '《建州胜筵》全景沉浸式主题餐秀',
         dot: {
-          top: '520rpx',
-          left: '350rpx'
+          top: '720rpx',
+          left: '550rpx'
         }
       },
       {
         id: 5,
         content: '《月映武夷》大型舞台秀',
         dot: {
-          top: '580rpx',
-          left: '480rpx'
+          top: '660rpx',
+          left: '400rpx'
         }
       },
       {
         id: 6,
         content: '南平建瓯市饕餮广场',
         dot: {
-          top: '500rpx',
-          left: '280rpx'
+          top: '940rpx',
+          left: '360rpx'
         }
       },
       {
         id: 7,
         content: '南平建盏文化创意园',
         dot: {
-          top: '540rpx',
-          left: '320rpx'
+          top: '520rpx',
+          left: '420rpx'
         }
       },
       {
         id: 8,
         content: '武夷山五夫古镇兴贤书院',
         dot: {
-          top: '620rpx',
-          left: '420rpx'
+          top: '500rpx',
+          left: '280rpx'
         }
       },
       {
         id: 9,
         content: '武夷山九曲溪山盟海誓浪漫游',
         dot: {
-          top: '600rpx',
-          left: '550rpx'
+          top: '740rpx',
+          left: '480rpx'
         }
       },
       {
         id: 10,
         content: '光泽杉关古驿道',
         dot: {
-          top: '480rpx',
-          left: '250rpx'
+          top: '580rpx',
+          left: '140rpx'
         }
       },
       {
         id: 11,
         content: '建阳崇阳溪漫游道',
         dot: {
-          top: '560rpx',
-          left: '380rpx'
+          top: '820rpx',
+          left: '460rpx'
         }
       },
       {
         id: 12,
         content: '考亭书院',
         dot: {
-          top: '540rpx',
-          left: '300rpx'
+          top: '830rpx',
+          left: '340rpx'
         }
       }
     ]
@@ -661,70 +661,70 @@ const CITY_CONFIG = {
     name: '龙岩',
     titleImage: 'https://app5.fjsen.com/h5/20260122/images/龙岩title.png',
     mapImage: 'https://app5.fjsen.com/h5/20260122/images/龙岩map.png',
-    textColumnsTop: '1150rpx', // 底部文字区域的top值
+    textColumnsTop: '1100rpx', // 底部文字区域的top值
     leftColumnCount: 4, // 左列文字条目数
     items: [{
         id: 1,
         content: '龙岩冠豸山九龙湖休闲区',
         dot: {
-          top: '560rpx',
-          left: '550rpx'
+          top: '740rpx',
+          left: '140rpx'
         }
       },
       {
         id: 2,
         content: '漳平市永福樱花茶园',
         dot: {
-          top: '620rpx',
-          left: '620rpx'
+          top: '510rpx',
+          left: '420rpx'
         }
       },
       {
         id: 3,
         content: '龙岩长汀古城" ⽩⽇·上房揭瓦"',
         dot: {
-          top: '500rpx',
-          left: '280rpx'
+          top: '620rpx',
+          left: '300rpx'
         }
       },
       {
         id: 4,
         content: '龙岩客家大院美食园',
         dot: {
-          top: '540rpx',
-          left: '350rpx'
+          top: '470rpx',
+          left: '520rpx'
         }
       },
       {
         id: 5,
         content: '龙岩非遗文创园',
         dot: {
-          top: '580rpx',
-          left: '420rpx'
+          top: '720rpx',
+          left: '490rpx'
         }
       },
       {
         id: 6,
         content: '永定土楼·客家香叙酒店',
         dot: {
-          top: '600rpx',
-          left: '500rpx'
+          top: '800rpx',
+          left: '420rpx'
         }
       },
       {
         id: 7,
         content: '上杭汀江绿道',
         dot: {
-          top: '520rpx',
-          left: '380rpx'
+          top: '630rpx',
+          left: '600rpx'
         }
       },
       {
         id: 8,
         content: '古田会议会址',
         dot: {
-          top: '480rpx',
-          left: '450rpx'
+          top: '590rpx',
+          left: '180rpx'
         }
       }
     ]
@@ -733,78 +733,78 @@ const CITY_CONFIG = {
     name: '宁德',
     titleImage: 'https://app5.fjsen.com/h5/20260122/images/宁德title.png',
     mapImage: 'https://app5.fjsen.com/h5/20260122/images/宁德map.png',
-    textColumnsTop: '1150rpx', // 底部文字区域的top值
+    textColumnsTop: '1080rpx', // 底部文字区域的top值
     leftColumnCount: 5, // 左列文字条目数
     items: [{
         id: 1,
         content: '宁德霞浦"洞见·蓝" 空间',
         dot: {
-          top: '540rpx',
-          left: '530rpx'
+          top: '780rpx',
+          left: '540rpx'
         }
       },
       {
         id: 2,
         content: '福鼎市绿雪芽非遗工坊',
         dot: {
-          top: '490rpx',
-          left: '600rpx'
+          top: '520rpx',
+          left: '490rpx'
         }
       },
       {
         id: 3,
         content: '嵛山镇海岛音乐会',
         dot: {
-          top: '530rpx',
-          left: '400rpx'
+          top: '560rpx',
+          left: '630rpx'
         }
       },
       {
         id: 4,
         content: '宁德霞浦太康路美食一条街',
         dot: {
-          top: '650rpx',
-          left: '465rpx'
+          top: '810rpx',
+          left: '390rpx'
         }
       },
       {
         id: 5,
         content: '屏南县一溪云公众空间',
         dot: {
-          top: '420rpx',
-          left: '400rpx'
+          top: '720rpx',
+          left: '110rpx'
         }
       },
       {
         id: 6,
         content: '周宁鲤鱼溪鱼祭广场',
         dot: {
-          top: '730rpx',
-          left: '430rpx'
+          top: '570rpx',
+          left: '260rpx'
         }
       },
       {
         id: 7,
         content: '福鼎嵛山岛环岛路',
         dot: {
-          top: '780rpx',
-          left: '480rpx'
+          top: '680rpx',
+          left: '540rpx'
         }
       },
       {
         id: 8,
         content: '宁德霞浦三沙花竹村观日地标',
         dot: {
-          top: '620rpx',
-          left: '220rpx'
+          top: '650rpx',
+          left: '310rpx'
         }
       },
       {
         id: 9,
         content: '下党乡红色研学实践教育基地',
         dot: {
-          top: '450rpx',
-          left: '270rpx'
+          top: '440rpx',
+          left: '370rpx'
         }
       }
     ]
@@ -819,64 +819,64 @@ const CITY_CONFIG = {
         id: 1,
         content: '平潭 68 海里景区"两岸同心窗"',
         dot: {
-          top: '580rpx',
-          left: '480rpx'
+          top: '830rpx',
+          left: '420rpx'
         }
       },
       {
         id: 2,
         content: '"岚起山海图"夜游演艺',
         dot: {
-          top: '640rpx',
-          left: '520rpx'
+          top: '990rpx',
+          left: '340rpx'
         }
       },
       {
         id: 3,
         content: '平潭孤独咖啡馆',
         dot: {
-          top: '520rpx',
-          left: '380rpx'
+          top: '450rpx',
+          left: '260rpx'
         }
       },
       {
         id: 4,
         content: '平潭橘树里海景民宿',
         dot: {
-          top: '680rpx',
-          left: '420rpx'
+          top: '560rpx',
+          left: '480rpx'
         }
       },
       {
         id: 5,
         content: '壳丘头遗址博物馆',
         dot: {
-          top: '480rpx',
-          left: '350rpx'
+          top: '440rpx',
+          left: '320rpx'
         }
       },
       {
         id: 6,
         content: '北部生态廊道',
         dot: {
-          top: '560rpx',
-          left: '280rpx'
+          top: '380rpx',
+          left: '370rpx'
         }
       },
       {
         id: 7,
         content: '平潭22号国际游艇码头',
         dot: {
-          top: '620rpx',
-          left: '320rpx'
+          top: '850rpx',
+          left: '550rpx'
         }
       },
       {
         id: 8,
         content: '中国水下考古·平潭展示体验馆',
         dot: {
-          top: '600rpx',
-          left: '400rpx'
+          top: '840rpx',
+          left: '170rpx'
         }
       }
     ]
